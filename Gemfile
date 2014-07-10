a486d95f99f7924e19d7af1cac3002a942d67d87
Roger Brown <rwbrown84@gmail.com>
2014-07-10 17:59:26 -0400

    Finished Day 24.2

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
 gem 'devise', '~> 3.2.4'
 gem 'newrelic_rpm'
+gem 'paperclip', github: 'thoughtbot/paperclip'
 
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -46,12 +46,12 @@ class PinsController < ApplicationController
     end
 
     def correct_user
-      @pin = current.user.pins.find_by(id: params[:id])
+      @pin = current_user.pins.find_by(id: params[:id])
       redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
